c113dd66f516cc93bd075e6b0214a8ac0b5b9937
Ing Wilson <nosliwsys@gmail.com>
2019-01-18 14:23:20 -0500

    initial commit

---
+++ app/src/main/java/com/a952000243/ingwilson/sesion1/MiAdaptador.java
@@ -0,0 +1,45 @@
+package com.a952000243.ingwilson.sesion1;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Vector;
+
+public class MiAdaptador extends
+        RecyclerView.Adapter<MiAdaptador.ViewHolder> {
+    private LayoutInflater inflador;
+    private Vector<String> lista;
+    public MiAdaptador(Context context, Vector<String> lista) {
+        this.lista = lista;
+        inflador = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = inflador.inflate(R.layout.miitem, parent, false);
+        return new ViewHolder(v);
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int i) {
+        holder.titulo.setText(lista.get(i));
+    }
+    @Override
+    public int getItemCount() {
+        return lista.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView titulo, subtitutlo;
+        public ImageView icon;
+        ViewHolder(View itemView) {
+            super(itemView);
+            titulo = (TextView)itemView.findViewById(R.id.titulo);
+            subtitutlo = (TextView)itemView.findViewById(R.id.subtitulo);
+            icon = (ImageView)itemView.findViewById(R.id.icono);
+        }
+    }
+}
